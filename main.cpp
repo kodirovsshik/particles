b8d504476dc2479ba9623c485492c89fedfc3661
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-03-20 02:13:17 +0300

    Kinda architecture remaster

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <ksn/stuff.hpp>
 #include <ksn/math_constants.hpp>
 #include <ksn/try_smol_buffer.hpp>
+#include <ksn/math_common.hpp>
 
 #include <stdarg.h>
 #include <string.h>
@@ -16,6 +17,9 @@
 #include <GL/glew.h>
 
 #include <chrono>
+#include <concepts>
+#include <algorithm>
+#include <random>
 
 #include <Windows.h>
 
@@ -65,6 +69,15 @@ struct particle_t
 	float vx, vy;
 	float ax, ay;
 	float radius, mass;
+#if _KSN_IS_DEBUG_BUILD
+	float debug_float[8];
+	uint32_t debug_uint[8];
+#endif
+
+	bool operator==(const particle_t& x) const noexcept
+	{
+		return memcmp(this, &x, sizeof(*this)) == 0;
+	}
 };
 
 static_assert(sizeof(float) == sizeof(cl_float));
@@ -73,53 +86,78 @@ static_assert(sizeof(float) == sizeof(cl_float));
 
 const char* const cl_sources[] =
 {
-	R"(
 
+	R"(
+	
 typedef struct
 {
 	float x, y;
 	float vx, vy;
 	float ax, ay;
 	float radius, mass;
+)"
+
+#if _KSN_IS_DEBUG_BUILD
+R"(
+	float debug_float[8];
+	unsigned int debug_uint[8];
+)"
+#endif
+R"(
 } particle_t;
 
-__kernel void interaction(__global particle_t* p, float dt)
+//Computes how p[j] affects all other p[i]
+__kernel void interaction(__global particle_t* p, float dt, unsigned int size, unsigned int j)
 {
-	__global particle_t* p1 = p + get_global_id(0);
-	__global particle_t* p2 = p + get_global_id(1);
+	unsigned int i = get_global_id(0);
+	if (i >= size) return;
 
-	if (p2 <= p1) return;
+	//p[i].debug_uint[0] = i;
+	//p[i].debug_uint[1] = j;
+	//p[i].debug_uint[2] = get_global_id(0);
+	//p[i].debug_uint[3] = get_global_size(0);
+	//p[i].debug_uint[4] = get_local_size(0);
+	//p[i].debug_uint[5] = get_local_id(0);
+	//p[i].debug_uint[6] = size;
+
+	if (i == j) return;
+
+	__global particle_t* p1 = p + i;
+	__global particle_t* p2 = p + j;
 
 	float dx = p2->x - p1->x;
 	float dy = p2->y - p1->y;
-	float r = dx * dx + dy * dy;
-	r = 1 / r;
+	float r_inv = (dx * dx + dy * dy);
+	r_inv = 1 / r_inv;
 
-	//float k = 6.6743e-11f * r;
-	float a1 = r * p2->mass;
-	float a2 = r * p1->mass;
+	float a1 = r_inv * p2->mass;
 
-	r = sqrt(r);
+	r_inv = sqrt(r_inv);
 	
-	float x_proj = dx * r;
-	float y_proj = dy * r;
+	float x_proj = dx * r_inv;
+	float y_proj = dy * r_inv;
 	
 	p1->ax += x_proj * a1;
 	p1->ay += y_proj * a1;
-
-	p2->ax -= x_proj * a2;
-	p2->ay -= y_proj * a2;
 }
 
-__kernel void movement(__global particle_t* arr, float dt)
+__kernel void movement(__global particle_t* arr, float dt, unsigned int size)
 {
-	__global particle_t* p = arr + get_global_id(0);
+	unsigned int i = get_global_id(0);
+	if (i >= size) return;
 
-	//dt *= 6.6743e-11f;
+	__global particle_t* p = arr + i;
+	//p->debug_float[0]
 
-	p->vx += p->ax * 6.6743e-11f * dt;
-	p->vy += p->ay * 6.6743e-11f * dt;
-	
+	float c = 6.6743e-11f * dt;
+
+	float friction = 0.3f * (1 / 6.6743e-11f);
+	p->ax -= p->vx * friction;
+	p->ay -= p->vy * friction;
+
+	p->vx += p->ax * c;
+	p->vy += p->ay * c;
+
 	p->x += p->vx * dt;
 	p->y += p->vy * dt;
 
@@ -127,73 +165,49 @@ __kernel void movement(__global particle_t* arr, float dt)
 	p->ay = 0;
 }
 
-__kernel void collision_resolution(__global particle_t* p)
+__kernel void collision_resolution(__global particle_t* p, unsigned int size)
 {
-	size_t n1 = get_global_id(0);
-	size_t n2 = get_global_id(1);
+	
+}
 
-	if (n1 <= n2) return;
+__kernel void border(__global particle_t* p, unsigned int size, float x1, float x2, float y1, float y2)
+{
+	unsigned int i = get_global_id(0);
+	if (i >= size) return;
 
-	__global particle_t* p1 = p + n1;
-	__global particle_t* p2 = p + n2;
+	p += i;
 
-	float k1;
-	float k2;
+	//p->debug_float[0] = x1;
+	//p->debug_float[1] = x2;
+	//p->debug_float[2] = y1;
+	//p->debug_float[3] = y2;
+	//p->debug_float[4] = p->x;
+	//p->debug_float[5] = p->y;
 
-	float dx = p2->x - p1->x;
-	float dy = p2->y - p1->y;
-
+	if (p->x < x1)
 	{
-		float r = sqrt(dx * dx + dy * dy);
-		{
-			float sr = p1->radius + p2->radius;
-			if (r >= sr) return;
-			r = (sr - r) / r;
-		}
-
-		float M1;
-		float M2;
-
-		{
-			float v1 = sqrt(p1->vx * p1->vx + p1->vy * p1->vy);
-			M1 = p1->mass * v1;
-		}
-		{
-			float v2 = sqrt(p2->vx * p2->vx + p2->vy * p2->vy);
-			M2 = p2->mass * v2;
-		}
-		if (M1 == 0 && M2 == 0)
-		{
-			M1 = M2 = 0.5;
-		}
-		else
-		{
-			float M = M1 + M2;
-			if (M != 0)
-			{
-				M1 /= M;
-				M2 /= M;
-			}
-		}
-		
-		//r *= 1.2;
-		k1 = r * M1;
-		k2 = r * M2;
+		p->x = x1 + (x1 - p->x);
+		if (p->vx < 0) p->vx *= -1;
 	}
-
-	p1->x -= dx * k1;
-	p1->y -= dy * k1;
-
-	p2->x += dx * k2;
-	p2->y += dy * k2;
-
-	p1->vx = 0;
-	p1->vy = 0;
-	p2->vx = 0;
-	p2->vy = 0;
+	if (p->x > x2)
+	{
+		p->x = x2 - (p->x - x2);
+		if (p->vx > 0) p->vx *= -1;
+	}
+	if (p->y < y1)
+	{
+		p->y = y1 + (y1 - p->y);
+		if (p->vy < 0) p->vy *= -1;
+	}
+	if (p->y > y2)
+	{
+		p->y = y2 - (p->y - y2);
+		if (p->vy > 0) p->vy *= -1;
+	}
 }
 
 )"
+
 };
 
 size_t cl_sources_lengthes[ksn::countof(cl_sources)];
@@ -258,7 +272,7 @@ void draw_circle(float x, float y, float r)
 	float r_x = r * view_data.width_ratio;
 	float r_y = r * view_data.height_ratio;
 
-	size_t n_points = size_t(r_x > r_y ? r_x : r_y);
+	size_t n_points = size_t(r_x > r_y ? r_x : r_y) * 2;
 	if (n_points < 8) n_points *= 3;
 	float a = 0;
 	const float da = 2 * KSN_PIf / n_points;
@@ -283,6 +297,19 @@ void GLAPIENTRY gl_debug_callback(GLenum, GLenum, GLuint code, GLenum, GLsizei, 
 	__debugbreak();
 }
 
+
+_KSN_BEGIN
+
+template<std::integral T>
+constexpr T align_up(T value, T alignment)
+{
+	T remainder = value % alignment;
+	if (remainder) value += alignment - remainder;
+	return value;
+	//Compiler, i hope for your CMOV support
+}
+
+_KSN_END
 
 
 int main()
@@ -354,7 +381,7 @@ int main()
 
 
 	printf("\nOpenGL:\nRunning OpenGL %s\n%s\n%s\n\n", glGetString(GL_VERSION), glGetString(GL_RENDERER), glGetString(GL_VENDOR));
-	do
+	if constexpr (true)
 	{
 		char buffer_version[80];
 		char buffer_device[80];
@@ -362,12 +389,12 @@ int main()
 		
 		tempi = 0;
 		clGetCommandQueueInfo(cl_q, CL_QUEUE_DEVICE, sizeof(cl_device), &cl_device, nullptr);
-		tempi |= clGetDeviceInfo(cl_device, CL_DEVICE_VERSION, 79, &buffer_version, nullptr);
-		tempi |= clGetDeviceInfo(cl_device, CL_DEVICE_NAME, 79, &buffer_device, nullptr);
-		tempi |= clGetDeviceInfo(cl_device, CL_DEVICE_VENDOR, 79, &buffer_vendor, nullptr);
+		tempi |= clGetDeviceInfo(cl_device, CL_DEVICE_VERSION, sizeof(buffer_version), &buffer_version, nullptr);
+		tempi |= clGetDeviceInfo(cl_device, CL_DEVICE_NAME, sizeof(buffer_device), &buffer_device, nullptr);
+		tempi |= clGetDeviceInfo(cl_device, CL_DEVICE_VENDOR, sizeof(buffer_vendor), &buffer_vendor, nullptr);
 		
 		if (tempi == 0) printf("OpenCL:\nRunning %s\n%s\n%s\n\n", buffer_version, buffer_device, buffer_vendor);
-	} while (false);
+	}
 
 
 
@@ -377,7 +404,8 @@ int main()
 	particles.reserve(64);
 
 	if (false) //n circular layers of particles
-	for (int i = 0, n = 5; i < n; ++i)
+	//{} else
+	for (int i = 0, n = 10; i < n; ++i)
 	{ 
 		const float r = 10;
 		const float R = 25;
@@ -402,6 +430,7 @@ int main()
 		}
 	}
 	if (false)
+		//{} else
 	{ //Just two particles
 		float distance = 100;
 		
@@ -416,11 +445,12 @@ int main()
 		particles.push_back(p);
 	}
 	if (false)
+		//{} else
 	{ //Rotation (ellipse-shaped)
 		particle_t center{};
-		center.x = width / 2;
-		center.y = height / 2;
-		center.mass = 1.5e16;
+		center.x = width / 2.f;
+		center.y = height / 2.f;
+		center.mass = 1.5e16f;
 		center.radius = 50;
 		particles.push_back(center);
 
@@ -432,14 +462,15 @@ int main()
 		obj.radius = 5;
 		particles.push_back(obj);
 	}
-	if (true)
+	if (false)
+		//{} else
 	{//Right triangle
 		float r = 200;
 		
 		float a = KSN_PIf / 2;
 		float da = KSN_PIf / 1.5f;
 
-		float xc = width / 2, yc = height / 2;
+		float xc = width / 2.f, yc = height / 2.f;
 
 		for (int i = 0; i < 3; ++i)
 		{
@@ -453,12 +484,47 @@ int main()
 			a += da;
 		}
 	}
+	if (false)
+		{ } else
+	{
+		constexpr static size_t N = 1000;
+		
+		std::random_device rd;
+		std::mt19937_64 engine(rd());
+		std::uniform_real_distribution<float> dist_x(0, width);
+		std::uniform_real_distribution<float> dist_y(0, height);
+
+		particle_t p{};
+		p.radius = 5;
+		p.mass = 1e13f;
+		for (size_t i = 0; i < N; ++i)
+		{
+			p.x = dist_x(engine);
+			p.y = dist_y(engine);
+			particles.push_back(p);
+		}
+	}
+	if (false)
+	//{} else
+	{
+		particle_t p{};
+		p.x = width / 2.f;
+		p.y = height / 2.f;
+		p.vx = 2000;
+		p.radius = 10;
+		p.mass = 1;
+
+		particles.push_back(p);
+	}
+
+	const std::vector<particle_t> const_particles(particles);
 
 
 
 	auto kernel_interact = clCreateKernel(cl_program, "interaction", nullptr);
 	auto kernel_movement = clCreateKernel(cl_program, "movement", nullptr);
 	auto kernel_collision_resolution = clCreateKernel(cl_program, "collision_resolution", nullptr);
+	auto kernel_border = clCreateKernel(cl_program, "border", nullptr);
 
 	auto particles_buffer = clCreateBuffer(cl_context, 0, particles.size() * sizeof(particle_t), nullptr, nullptr);
 
@@ -470,7 +536,7 @@ int main()
 
 
 	win.show();
-	win.set_vsync_auto_or_enabled(true);
+	//win.set_vsync_auto_or_enabled(true);
 
 
 	std::vector<bool> key_pressed((int)ksn::keyboard_button_t::buttons_count, false);
@@ -482,12 +548,44 @@ int main()
 
 	float dt_fps = 1.f / 60;
 
-	size_t precision = 100;
+	size_t precision = 2;
 
 	auto clock_f = &std::chrono::system_clock::now;
 	auto t = clock_f();
 
-	//TODO: make gravity work well
+	size_t max_work_group_size = 0;
+	clGetDeviceInfo(cl_device, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(max_work_group_size), &max_work_group_size, nullptr);
+	const size_t local_work_size[] = { max_work_group_size};
+
+	uint32_t particles_size = (uint32_t)particles.size();
+	size_t global_work_size[] = { ksn::align_up<size_t>(particles_size, max_work_group_size) };
+
+	tempi = clSetKernelArg(kernel_interact, 0, sizeof(particles_buffer), &particles_buffer);
+	tempi = clSetKernelArg(kernel_interact, 2, sizeof(particles_size), &particles_size);
+
+	tempi = clSetKernelArg(kernel_movement, 0, sizeof(particles_buffer), &particles_buffer);
+	tempi = clSetKernelArg(kernel_movement, 2, sizeof(particles_size), &particles_size);
+
+	tempi = clSetKernelArg(kernel_collision_resolution, 0, sizeof(particles_buffer), &particles_buffer);
+	tempi = clSetKernelArg(kernel_collision_resolution, 1, sizeof(particles_size), &particles_size);
+
+	tempi = clSetKernelArg(kernel_border, 0, sizeof(particles_buffer), &particles_buffer);
+	tempi = clSetKernelArg(kernel_border, 1, sizeof(particles_size), &particles_size);
+	{
+		float temp;
+
+		temp = 0;
+		tempi = clSetKernelArg(kernel_border, 2, sizeof(temp), &temp);
+		temp = width;
+		tempi = clSetKernelArg(kernel_border, 3, sizeof(temp), &temp);
+		temp = 0;
+		tempi = clSetKernelArg(kernel_border, 4, sizeof(temp), &temp);
+		temp = height;
+		tempi = clSetKernelArg(kernel_border, 5, sizeof(temp), &temp);
+	}
+
+	clEnqueueWriteBuffer(cl_q, particles_buffer, CL_FALSE, 0, particles.size() * sizeof(particle_t), particles.data(), 0, nullptr, nullptr);
+
 	do
 	{
 		auto t1 = clock_f();
@@ -498,30 +596,48 @@ int main()
 		{
 			float dt_prec = dt_fps / precision;
 
-			clEnqueueWriteBuffer(cl_q, particles_buffer, CL_FALSE, 0, particles.size() * sizeof(particle_t), particles.data(), 0, nullptr, nullptr);
-
-			tempi = clSetKernelArg(kernel_interact, 0, sizeof(particles_buffer), &particles_buffer);
 			tempi = clSetKernelArg(kernel_interact, 1, sizeof(dt_prec), &dt_prec);
-
-			tempi = clSetKernelArg(kernel_movement, 0, sizeof(particles_buffer), &particles_buffer);
 			tempi = clSetKernelArg(kernel_movement, 1, sizeof(dt_prec), &dt_prec);
 
-			tempi = clSetKernelArg(kernel_collision_resolution, 0, sizeof(particles_buffer), &particles_buffer);
-
-			size_t global_work_offset[] = { 0, 0 };
-			size_t local_work_size[] = { 1, 1 };
-			size_t global_work_size[] = { particles.size(), particles.size() };
-
-			for (size_t i = 0; i < precision; ++i)
-			{
-				tempi = clEnqueueNDRangeKernel(cl_q, kernel_interact, 2, global_work_offset, global_work_size, local_work_size, 0, nullptr, nullptr);
-				tempi = clEnqueueNDRangeKernel(cl_q, kernel_movement, 1, global_work_offset, global_work_size, local_work_size, 0, nullptr, nullptr);
-				tempi = clEnqueueNDRangeKernel(cl_q, kernel_collision_resolution, 2, global_work_offset, global_work_size, local_work_size, 0, nullptr, nullptr);
-				tempi = clEnqueueNDRangeKernel(cl_q, kernel_collision_resolution, 2, global_work_offset, global_work_size, local_work_size, 0, nullptr, nullptr);
-			}
-			tempi = clEnqueueReadBuffer(cl_q, particles_buffer, CL_TRUE, 0, particles.size() * sizeof(particle_t), particles.data(), 0, nullptr, nullptr);
+			size_t global_work_offset[2] = { 0, 0 };
 			
-			clFlush(cl_q);
+			for (size_t j = 0; j < precision; ++j)
+			{
+				for (size_t i = 0; i < particles_size; ++i)
+				{
+					tempi = (int)i;
+					tempi = clSetKernelArg(kernel_interact, 3, sizeof(tempi), &tempi);
+					tempi = clEnqueueNDRangeKernel(cl_q, kernel_interact, 1, global_work_offset, global_work_size, local_work_size, 0, NULL, NULL);
+					//clFlush(cl_q);
+
+					//if constexpr (
+					//	false && 
+					//	_KSN_IS_DEBUG_BUILD)
+					//{
+					//	tempi = clEnqueueReadBuffer(cl_q, particles_buffer, CL_TRUE, 0, particles.size() * sizeof(particle_t), particles.data(), 0, nullptr, nullptr);
+					//	auto* mismatch_elem = &*std::mismatch(particles.begin(), particles.end(), const_particles.begin()).first;
+					//	size_t mismatch_index = mismatch_elem - particles.data();
+					//	if (mismatch_index == particles_size) mismatch_index = -1;
+					//	ksn::nop();
+					//}
+				}
+
+				clEnqueueNDRangeKernel(cl_q, kernel_movement, 1, global_work_offset, global_work_size, local_work_size, 0, NULL, NULL);
+				//clFlush(cl_q);
+				//_KSN_DEBUG_EXPR(tempi = clEnqueueReadBuffer(cl_q, particles_buffer, CL_TRUE, 0, particles.size() * sizeof(particle_t), particles.data(), 0, nullptr, nullptr));
+
+				clEnqueueNDRangeKernel(cl_q, kernel_border, 1, global_work_offset, global_work_size, local_work_size, 0, NULL, NULL);
+				//clFlush(cl_q);
+				//_KSN_DEBUG_EXPR(tempi = clEnqueueReadBuffer(cl_q, particles_buffer, CL_TRUE, 0, particles.size() * sizeof(particle_t), particles.data(), 0, nullptr, nullptr));
+
+				ksn::nop();
+			}
+
+			
+			tempi = clEnqueueReadBuffer(cl_q, particles_buffer, CL_TRUE, 0, particles.size() * sizeof(particle_t), particles.data(), 0, nullptr, nullptr);
+			ksn::nop();
+
+			printf("%i/10 ms\n", int(dt * 10000));
 		}
 
 
@@ -588,6 +704,65 @@ int main()
 		view_move_screen(view_delta_x, view_delta_y);
 
 	} 	while (win.is_open());
+	
+	
 
 	return 0;
 }
+
+
+
+//OpenCL float atomics (fucking slow)
+
+/*
+// inline void atomic_add_gf2(volatile __global float *addr, float val)
+//{
+//	union
+//	{
+//		unsigned int u32;
+//		float f32;
+//	} next, expected, current;
+//	current.f32 = *addr;
+//	do
+//	{
+//		expected.f32 = current.f32;
+//		next.f32 = expected.f32 + val;
+//		current.u32 = atomic_cmpxchg( (volatile __global unsigned int *)addr,
+//		expected.u32, next.u32);
+//	} while( current.u32 != expected.u32 );
+//}
+//
+//inline float atomic_xchg_gf(__global float* addr, float val)
+//{
+//	int tmp = atomic_xchg((__global int*)addr, *(__private int*)&val);
+//	return *(float*)&tmp;
+//}
+//
+//static float atomic_cmpxchg_gf(volatile __global float *p, float cmp, float val) {
+//	union {
+//		unsigned int u32;
+//		float        f32;
+//	} cmp_union, val_union, old_union;
+//
+//	cmp_union.f32 = cmp;
+//	val_union.f32 = val;
+//	old_union.u32 = atomic_cmpxchg((volatile __global unsigned int *) p, cmp_union.u32, val_union.u32);
+//	return old_union.f32;
+//}
+//
+//static float atomic_add_gf(volatile __global float *p, float val) {
+//	float found = *p;
+//	float expected;
+//	do {
+//		expected = found;
+//		found = atomic_cmpxchg_gf(p, expected, expected + val);
+//	} while (found != expected);
+//	return found;
+//}
+//
+// inline float atomic_add_gf1(volatile __global float *addr, float val)
+//{
+//	float data = *addr + val;
+//	return atomic_xchg_gf(addr, data);
+//}
+*/
